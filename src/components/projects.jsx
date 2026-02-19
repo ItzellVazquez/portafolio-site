d7826cda07f1f7234ffc7e764039bf8c0191b7fa
itzellvazquez-vinn <itzell.vazquez@vinneren.com.mx>
2026-02-18 18:45:45 -0600

    feat: se agrega contenido

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,13 +2,12 @@ import { Disclosure } from '@headlessui/react'
 
 const Projects = () => {
      return (
-    <div className="w-full px-4">
-        <span className='text-lg'>Proyectos</span>
+    <div className="w-full px-4 overflow-y-auto">
       <div className="mx-auto w-full max-w-md p-2">
         <Disclosure as="div" className="mt-2">
           {({ open }) => (
-            <div className="border-b-2 border-b-white-500">
-              <Disclosure.Button className="flex w-full justify-between px-4 py-2 text-left text-sm font-medium text-white">
+            <div className="border-b-1 border-b-white-500">
+              <Disclosure.Button className="flex w-full justify-between py-2 text-left text-sm font-medium text-white">
                 <span>Sistema Escolar</span>
               </Disclosure.Button>
               <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-white">
@@ -19,8 +18,8 @@ const Projects = () => {
         </Disclosure>
         <Disclosure as="div" className="mt-2">
           {({ open }) => (
-            <div className="border-b-2 border-b-white-500">
-              <Disclosure.Button className="flex w-full justify-between px-4 py-2 text-left text-sm font-medium text-white">
+            <div className="border-b-1 border-b-white-500">
+              <Disclosure.Button className="flex w-full justify-between py-2 text-left text-sm font-medium text-white">
                 <span>Tienda Online</span>
               </Disclosure.Button>
               <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-white">
@@ -31,8 +30,8 @@ const Projects = () => {
         </Disclosure>
         <Disclosure as="div" className="mt-2">
           {({ open }) => (
-            <div className="border-b-2 border-b-white-500">
-              <Disclosure.Button className="flex w-full justify-between px-4 py-2 text-left text-sm font-medium text-white">
+            <div className="border-b-1 border-b-white-500">
+              <Disclosure.Button className="flex w-full justify-between py-2 text-left text-sm font-medium text-white">
                 <span>Proyectos Vtex IO</span>
               </Disclosure.Button>
               <Disclosure.Panel className="px-4 pb-2 pt-4 text-sm text-white">
