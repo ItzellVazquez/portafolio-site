2ee00dbd1ef986195a918edd8763f3299a7e0705
itzellvazquez-vinn <itzell.vazquez@vinneren.com.mx>
2026-02-23 11:21:36 -0600

    feat: image & title + info section about

--- src/components/Sections/Hero.jsx
+++ src/components/Sections/Hero.jsx
@@ -2,7 +2,16 @@ import CardTransparent from "../CardTransparent"
 const Hero = () => {
     return (
         <CardTransparent>
-            <span>HERO</span>
+            <div className="flex flex-row justify-between items-center h-full">
+                <div className="flex flex-col justify-center items-start">
+                    <p className="text-6xl">Itzell Vazquez</p>
+                    <p className="text-4xl text-[#35A9DE]">Front-end Developer</p>
+                    <p className="text-3xl">VTEX IO & E-commerce</p>
+                </div>
+                <div className="flex flex-col justify-center items-end">
+                    <img className="h-full max-h-100" src="/src/assets/itzell.jpeg" alt="Itzell Vazquez"></img>
+                </div>
+            </div>
         </CardTransparent>
     )
 }
